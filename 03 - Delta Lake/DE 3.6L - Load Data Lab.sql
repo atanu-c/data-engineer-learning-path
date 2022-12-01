f60ed935dd592d33d5def1858feeb6f89ff078e4
atanu-c <atanu-c>
2022-12-01 23:32:23 +0000

    3.6L

--- 03 - Delta Lake/DE 3.6L - Load Data Lab.sql
+++ 03 - Delta Lake/DE 3.6L - Load Data Lab.sql
@@ -64,7 +64,15 @@
 -- COMMAND ----------
 
 -- TODO
-<FILL_IN>
+-- <FILL_IN>
+CREATE OR REPLACE TABLE events_raw (
+  key BINARY, 
+  offset LONG, 
+  partition INTEGER,
+  timestamp LONG,
+  topic STRING,
+  value BINARY
+  )
 
 -- COMMAND ----------
 
@@ -103,7 +111,9 @@
 -- COMMAND ----------
 
 -- TODO
-<FILL_IN>
+-- <FILL_IN>
+INSERT INTO events_raw
+SELECT * FROM events_json
 
 -- COMMAND ----------
 
@@ -115,7 +125,9 @@
 -- COMMAND ----------
 
 -- TODO
-<FILL_IN>
+-- <FILL_IN>
+SELECT *
+FROM events_raw
 
 -- COMMAND ----------
 
@@ -154,7 +166,9 @@
 -- COMMAND ----------
 
 -- TODO
-<FILL_IN> ${da.paths.datasets}/ecommerce/raw/item-lookup
+-- <FILL_IN>
+CREATE OR REPLACE TABLE item_lookup AS
+SELECT * FROM parquet.`${da.paths.datasets}/ecommerce/raw/item-lookup`;
 
 -- COMMAND ----------
 
